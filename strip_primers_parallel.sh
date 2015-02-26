f09b90e7ff215469b2505e3dac5535e131e69c0f
alk224 <alk224@nau.edu>
2015-02-25 17:49:52 -0700

    vastly improved functionality.  might be useable now...

--- strip_primers_parallel.sh
+++ strip_primers_parallel.sh
@@ -5,7 +5,9 @@ set -e
 	if [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
 		echo "
 		Usage (order is important!):
-		strip_primers_parallel <fastq1> <fastq2> <rev/comp primers as fasta> <threads to use>
+		strip_primers_parallel <rev/comp_primers> <threads> <read1> <read2> <index1> <index2>
+
+		<index2> is optional.
 
 		Resulting files will be output to a subdirectory called fastq-mcf_out.
 
@@ -32,127 +34,210 @@ set -e
 		exit 0
 	fi
 
-# if other than four arguments supplied, display usage 
-	if [  $# -ne 4 ]; then 
+## If other than five or six arguments supplied, display usage
+
+	if [[ $# -le 4 ]] || [[ $# -ge 7 ]]; then 
 		echo "
 		Usage (order is important!):
-		strip_primers_parallel <fastq1> <fastq2> <rev/comp primers as fasta> <threads to use>
+		strip_primers_parallel <rev/comp_primers> <threads> <read1> <read2> <index1> <index2>
 
-		Resulting files will be output to the same directory.
+		<index2> is optional.
+
+		Resulting files will be output to a subdirectory called fastq-mcf_out.
 		"
 		exit 1
 	fi 
   
-		#Determine number of sequences in input file 
+	workdir=$(pwd)
+	res1=$(date +%s.%N)
 
-		echo "
-		Reading input files...
+## Check for output directory
+
+	if [[ ! -d $workdir/fastq-mcf_out ]]; then
+
+		mkdir -p $workdir/fastq-mcf_out
+
+	else
+		echo "		
+		Directory fastq-mcf_output exists.
+		Attempting to use previously generated files.
 		"
+	fi
 
-		fastqlines=$(cat $1 | wc -l)
-		fastqseqs=$(($fastqlines/4))
-		corelines=$(($fastqseqs/$4))
-		digits=$(grep -o \. <<<$corelines | wc -l)
+	outdir=$workdir/fastq-mcf_out
+	primers=($1)
+	cores=($2)
+	read1=($3)
+	read2=($4)
+	index1=($5)
+	index2=($6)
+	read1name=$( basename $read1 )
+	read1base=`basename "$read1" | cut -d. -f1`
+	read1ext="${read1##*.}"
+	read2name=$( basename $read2 )
+	read2base=`basename "$read2" | cut -d. -f1`
+	read2ext="${read2##*.}"
+	index1name=$( basename $index1 )
+	index1base=`basename "$index1" | cut -d. -f1`
+	index1ext="${read1##*.}"
+	if [[ ! -z $index2 ]]; then
+	index2name=$( basename $index2 )
+	index2base=`basename "$index2" | cut -d. -f1`
+	index2ext="${read2##*.}"
+	fi
+	date0=`date +%Y%m%d_%I%M%p`
+	log=($outdir/fastq-mcf_$date0.log)
+
+## Check input files
+
+	if [[ ! -f $index1 ]]; then
+	echo "	$index1 is not a valid file.
+	Exiting.
+	"
+	exit 1
+	elif [[ ! -f $read1 ]]; then
+	echo "	$read1 is not a valid file.
+	Exiting.
+	"
+	exit 1
+	elif [[ ! -f $read2 ]]; then
+	echo "	$read2 is not a valid file.
+	Exiting.
+	"
+	exit 1
+	fi
+	if [[ ! -z $index2 ]]; then
+	if [[ ! -f $index2 ]]; then
+	echo "	$index2 is not a valid file.
+	Exiting.
+	"
+	exit 1
+	fi
+	fi
 
 ## Check for required dependencies:
 
 	scriptdir="$( cd "$( dirname "$0" )" && pwd )"
 
-echo "
-		Checking for required dependencies...
-"
+	echo "		Checking for required dependencies...
+	"
 
-scriptdir="$( cd "$( dirname "$0" )" && pwd )"
+	scriptdir="$( cd "$( dirname "$0" )" && pwd )"
 
 
-for line in `cat $scriptdir/akutils_resources/strip_primers.dependencies.list`; do
+	for line in `cat $scriptdir/akutils_resources/strip_primers.dependencies.list`; do
 	dependcount=`command -v $line 2>/dev/null | wc -w`
-	if [[ $dependcount == 0 ]]; then
-	echo "
+		if [[ $dependcount == 0 ]]; then
+		echo "
 		$line is not in your path.  Dependencies not satisfied.
 		Exiting.
+		"
+		exit 1
+		else
+		if [[ $dependcount -ge 1 ]]; then
+		echo "		$line is in your path..."
+		fi
+		fi
+	done
+	echo "		All dependencies satisfied.  Proceeding...
 	"
-	exit 1
-	else
-	if [[ $dependcount -ge 1 ]]; then
-	echo "		$line is in your path..."
-	fi
-	fi
-done
-echo "
-		All dependencies satisfied.  Proceeding...
-"
 
-## set working directory, move there, and check for existing outputs
+## Set working directory
 
-	res1=$(date +%s.%N)
-	workdir=$(pwd)
-	cd $workdir
-
-	if [[ ! -d $workdir/fastq-mcf_out ]]; then
-
-		mkdir $workdir/fastq-mcf_out
-
+	if [[ ! -d $outdir ]]; then
+		mkdir -p $outdir
 	else
 		echo "		Directory fastq-mcf_output exists.
 		Deleting contents and filtering data.
 		"
-		rm -r fastq-mcf_out/*
-
+		rm -r fastq-mcf_out
+		mkdir -p $outdir
 	fi
 
-		outdir=$workdir/fastq-mcf_out
+## Parallel processing steps
 
-	if [ "$digits" -lt "4" ]; then
+	echo "		Processing on $cores threads...
+	"
 
-		echo "		Your fastq input has fewer than 10,000 sequences.  
-		Processing on a single core only.
-		"
+	if [[ $read1ext != "fastq" ]]; then
+	mv $read1 $read1base.fastq
+	fi
+	if [[ $read2ext != "fastq" ]]; then
+	mv $read2 $read2base.fastq
+	fi
+	if [[ $index1ext != "fastq" ]]; then
+	mv $index1 $index1base.fastq
+	fi
+	if [[ ! -z $index2 ]]; then
+	if [[ $index2ext != "fastq" ]]; then
+	mv $index2 $index2base.fastq
+	fi
+	fi
 
-		#extract filename bases for output naming purposes
-		fastq1base=`basename "$1" | cut -d. -f1`
-		fastq2base=`basename "$2" | cut -d. -f1`
-		#fastq-mcf command (single process)
-		`fastq-mcf -0 -t 0.0001 $3 $1 $2 -o $outdir/$fastq1base.mcf.fq -o $outdir/$fastq2base.mcf.fq > $outdir/fastq-mcf.log`
-	else
-		echo "
-		Processing on $4 threads...
-		"
+	#Use fasta-splitter.pl to split fastq files according to desired processing level
+	echo "		Splitting input sequences.
+	"
 
-		# make temp dir
-		mkdir $outdir/mcf-temp
-		#make log file to compile all logged removals into
-		echo > $outdir/fastq-mcf.log
+	( fastq-splitter.pl --n-parts $cores --measure count $read1base.fastq ) &
+	( fastq-splitter.pl --n-parts $cores --measure count $read2base.fastq ) &
+	wait
 
-		#use fastqutils command (NGSutils) to split fastq files according to desired processing level		
-		`fastqutils split $1 $outdir/mcf-temp/r1.temp $4`
-		`fastqutils split $2 $outdir/mcf-temp/r2.temp $4`
-		wait
+	echo "
+		Stripping primers from data.
+	"
 
-	#Parallel processing of fastq-mcf commands in background
-	for splitseq in $outdir/mcf-temp/r1.*.fastq; do	
-		( splitbase=$(basename $splitseq .fastq)
-		splitbase2=$(echo $splitbase | sed 's/r1/r2/g')
-		fastq-mcf -0 -t 0.0001 $3 $outdir/mcf-temp/$splitbase.fastq $outdir/mcf-temp/$splitbase2.fastq -o $outdir/mcf-temp/$splitbase.mcf.fastq -o $outdir/mcf-temp/$splitbase2.mcf.fastq >> $outdir/fastq-mcf.log ) &
+	for rd1part in `ls $read1base.part-*.fastq` ; do
+		part=`ls $rd1part | cut -d. -f2`
+		mkdir $outdir/$part
+		mv $read1base.$part.fastq $outdir/$part/
+		mv $read2base.$part.fastq $outdir/$part/
+		( fastq-mcf -0 -t 0.0001 $primers $outdir/$part/$read1base.$part.fastq $outdir/$part/$read2base.$part.fastq -o $outdir/$part/$read1base.$part.mcf.fastq -o $outdir/$part/$read2base.$part.mcf.fastq >> $log ) &
 	done
 	wait
 
 	#Cat results together
-		cat $outdir/mcf-temp/r1.temp.*.mcf.fastq > $outdir/r1.mcf.fastq
-		cat $outdir/mcf-temp/r2.temp.*.mcf.fastq > $outdir/r2.mcf.fastq
+		cat $outdir/part-*/$read1base.part-*.mcf.fastq > $outdir/$read1base.mcf.fastq
+		cat $outdir/part-*/$read2base.part-*.mcf.fastq > $outdir/$read2base.mcf.fastq
 		wait
-	#Remove temp files
-		rm -r $outdir/mcf-temp
-	fi
-		echo "		Processing complete.  Filtered data is found in the 
-		following output files:
-		
-		$outdir/r1.mcf.fastq
-		$outdir/r2.mcf.fastq
 
-		Details can be found in $outdir/fastq-mcf.log
+## Check for and remove empty fastq records
 
-		"
+#	echo "
+#Filtering empty fastq records from input files." >> $log
+#date "+%a %b %I:%M %p %Z %Y" >> $log
+	echo "		Filtering empty fastq records from input files.
+	"
+
+		emptycount=`grep -e "^$" $outdir/$read1base.mcf.fastq | wc -l`
+
+		if [[ $emptycount != 0 ]]; then
+
+		grep -B 1 -e "^$" $outdir/$read1base.mcf.fastq > $outdir/empty.fastq.records
+#		grep -B 1 -e "^$" $outdir/$read2base.mcf.fastq >> $outdir/empty.fastq.records
+		sed -i '/^\s*$/d' $outdir/empty.fastq.records
+		sed -i '/^\+/d' $outdir/empty.fastq.records
+		sed -i '/^\--/d' $outdir/empty.fastq.records
+		sed -i 's/^\@//' $outdir/empty.fastq.records
+		empties=`cat $outdir/empty.fastq.records | wc -l`
+#	echo "
+#Found $empties empty fastq records." >> $log
+	echo "		Found $empties empty fastq records.
+	"
+
+		( filter_fasta.py -f $outdir/$read1base.mcf.fastq -o $outdir/$read1base.noprimers.fastq -s $outdir/empty.fastq.records -n ) &
+		( filter_fasta.py -f $outdir/$read2base.mcf.fastq -o $outdir/$read2base.noprimers.fastq -s $outdir/empty.fastq.records -n ) &
+		( filter_fasta.py -f $index1base.fastq -o $outdir/$index1base.noprimers.fastq -s $outdir/empty.fastq.records -n ) &
+		if [[ ! -z $index2 ]]; then
+		( filter_fasta.py -f $index2base.fastq -o $outdir/$index2base.noprimers.fastq -s $outdir/empty.fastq.records -n ) &
+		fi
+		wait
+		fi
+
+## Remove temp files
+	rm -r $outdir/part-*
+	rm $outdir/$read1base.mcf.fastq
+	rm $outdir/$read2base.mcf.fastq
+	rm $outdir/empty.fastq.records
 
 ## Log end of workflow
 
@@ -167,8 +252,7 @@ ds=$(echo "$dt3-60*$dm" | bc)
 
 runtime=`printf "Total runtime: %d days %02d hours %02d minutes %02.1f seconds\n" $dd $dh $dm $ds`
 
-echo "
-		Strip primers workflow steps completed.
+echo "		Strip primers workflow steps completed.
 
 		$runtime
 "
