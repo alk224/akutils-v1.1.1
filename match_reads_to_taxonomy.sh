2f02ee93e2ed52c5113f30021a0ce9e40d6d02d4
alk224 <alk224@nau.edu>
2015-10-01 16:41:54 -0700

    testing changes for Centos compatibility

--- match_reads_to_taxonomy.sh
+++ match_reads_to_taxonomy.sh
@@ -81,88 +81,72 @@ fi
 rep_set=`ls $outdir/Representative_sequences | grep "rep_set.fna"`
 
 ## convert rarefied OTU table to .txt
-
 if [[ ! -f $outdir/OTU_tables/$tablename.txt ]]; then
 	biomtotxt.sh $1 &>/dev/null
 fi
 table=$outdir/OTU_tables/$tablename.txt
 
 ## get column from OTU table that contains the taxonomy string
-
 taxa_column=`awk -v outdir="$outdir" -v table="$table" '{ for(i=1;i<=NF;i++){if ($i ~ /taxonomy/) {print i}}}' $table`
 alt_taxa_column=`expr $taxa_column - 1`
 
 ## sort OTU table by taxonomy
-
-if [[ ! -f $outdir/OTU_tables/$tablename\_sorted_by_taxonomy.txt ]]; then
-	head -2 $outdir/OTU_tables/$tablename.txt | tail -1 > $outdir/OTU_tables/$tablename\_sorted_by_taxonomy.txt
-	tail -n +3 $outdir/OTU_tables/$tablename.txt | sort -k$taxa_column >> $outdir/OTU_tables/$tablename\_sorted_by_taxonomy.txt
+if [[ ! -f $outdir/OTU_tables/${tablename}_sorted_by_taxonomy.txt ]]; then
+	head -2 $outdir/OTU_tables/$tablename.txt | tail -1 > $outdir/OTU_tables/${tablename}_sorted_by_taxonomy.txt
+	tail -n +3 $outdir/OTU_tables/$tablename.txt | sort -k$taxa_column >> $outdir/OTU_tables/${tablename}_sorted_by_taxonomy.txt
 fi
 
 ## make list of OTUs from sorted table
-
 if [[ ! -f $outdir/OTU_tables/$tablename\_sorted_by_taxonomy_otu_list.txt ]]; then
-	grep -v "#" $outdir/OTU_tables/$tablename\_sorted_by_taxonomy.txt | cut -f1 > $outdir/OTU_tables/$tablename\_sorted_by_taxonomy_otu_list.txt
+	grep -v "#" $outdir/OTU_tables/${tablename}_sorted_by_taxonomy.txt | cut -f1 > $outdir/OTU_tables/${tablename}_sorted_by_taxonomy_otu_list.txt
 fi
 
-## Check for prior outputs
-
-#if [[ ! -d $outdir/Representative_sequences ]]; then
-#	mkdir $outdir/Representative_sequences
-#fi
-
-#Rep_seqs_file_count=`ls $outdir/Representative_sequences/ | wc -l`
-#if [[ Rep_seqs_file_count -ge 1 ]]; then
-#	rm -r $outdir/Representative_sequences/*
-#fi
-
 ## Build sequence file, adding taxonomy string to fasta header
-
 for otuid in `cat $outdir/OTU_tables/$tablename\_sorted_by_taxonomy_otu_list.txt`; do 
-	grep -A 1 -e ">$otuid\s" $outdir/Representative_sequences/$rep_set >> $outdir/Representative_sequences/$tablename\_rep_sequences.fasta
-	sed -i "/^>$otuid\s/ s/$otuid\s/$otuid\t/" $outdir/Representative_sequences/$tablename\_rep_sequences.fasta
-	otuid_tax_string0=`grep -e "$otuid\s" $outdir/OTU_tables/$tablename\_sorted_by_taxonomy.txt | cut -f$alt_taxa_column`
+	grep -A 1 -e ">$otuid\s" $outdir/Representative_sequences/$rep_set >> $outdir/Representative_sequences/${tablename}_rep_sequences.fasta
+	sed -i "/^>$otuid\s/ s/$otuid\s/$otuid\t/" $outdir/Representative_sequences/${tablename}_rep_sequences.fasta
+	otuid_tax_string0=`grep -e "$otuid\s" $outdir/OTU_tables/${tablename}_sorted_by_taxonomy.txt | cut -f$alt_taxa_column`
 	otuid_tax_string1=`echo $otuid_tax_string0 | sed "s/; /__/g"`
 	otuid_tax_string=`echo $otuid_tax_string1 | sed "s/ /_/g"`
-	sed -i "/^>$otuid\t/ s/$/\t$otuid_tax_string/" $outdir/Representative_sequences/$tablename\_rep_sequences.fasta
+	sed -i "/^>$otuid\t/ s/$/\t$otuid_tax_string/" $outdir/Representative_sequences/${tablename}_rep_sequences.fasta
 done
+wait
 
 ## strip out characters that are stupidly used in greengenes strings
-sed -i -e "s/\[//g" -e "s/\]//g" -e "s/'//g" $outdir/Representative_sequences/$tablename\_rep_sequences.fasta
+sed -i -e "s/\[//g" -e "s/\]//g" -e "s/'//g" $outdir/Representative_sequences/${tablename}_rep_sequences.fasta
+wait
 
 ## Build taxonomy list from L7 table and L7 table with matching taxonomy strings
-
 cp $outdir/taxa_plots/table_sorted_L7.txt $outdir/Representative_sequences/L7_table0.txt
 grep "#OTU ID" $outdir/Representative_sequences/L7_table0.txt > $outdir/Representative_sequences/L7_table.txt
 grep -v "#" $outdir/Representative_sequences/L7_table0.txt | sed "s/;/__/g" | sed "s/ /_/g" >> $outdir/Representative_sequences/L7_table.txt
 rm $outdir/Representative_sequences/L7_table0.txt
-
 grep -v "#" $outdir/Representative_sequences/L7_table.txt | cut -f1 | sed "s/;/__/g" | sed "s/ /_/g" > $outdir/Representative_sequences/L7_taxa_list.txt
+wait
 
 ## strip out annoying characters found in greengenes tax strings
 sed -i -e "s/\[//g" -e "s/\]//g" -e "s/'//g" $outdir/Representative_sequences/L7_taxa_list.txt
+wait
 
 ## remove "__Other" string from less confident tax assignments in taxa list for searching purposes
 sed -i "s/__Other.*//" $outdir/Representative_sequences/L7_taxa_list.txt
+wait
 
 ## Add number of OTUs to second column of taxa list
-
 for taxid in `cat $outdir/Representative_sequences/L7_taxa_list.txt`; do
-	num_otus=`grep -Fwc "$taxid" $outdir/Representative_sequences/$tablename\_rep_sequences.fasta`
+	num_otus=`grep -Fwc "$taxid" $outdir/Representative_sequences/${tablename}_rep_sequences.fasta`
 	#echo $num_otus
 	sed -i "/${taxid}$/ s/$/\t${num_otus}/" $outdir/Representative_sequences/L7_taxa_list.txt
 done
+wait
 
 ## Build taxon-specific multi-fasta files
-
 mkdir -p $outdir/Representative_sequences/L7_sequences_by_taxon
 for taxid in `cat $outdir/Representative_sequences/L7_taxa_list.txt | cut -f1`; do
-
-	grep -A 1 -w "$taxid" $outdir/Representative_sequences/$tablename\_rep_sequences.fasta > $outdir/Representative_sequences/L7_sequences_by_taxon/$taxid.fasta
+	grep -A 1 -w "$taxid" $outdir/Representative_sequences/${tablename}_rep_sequences.fasta > $outdir/Representative_sequences/L7_sequences_by_taxon/$taxid.fasta
 done
 
 ## Mafft alignments for taxa with multiple reads per OTU -- copy single representative reads
-
 mkdir -p $outdir/Representative_sequences/L7_sequences_by_taxon_alignments
 for taxid in `cat $outdir/Representative_sequences/L7_taxa_list.txt | cut -f1`; do
 	otu_count=`grep -w "$taxid" $outdir/Representative_sequences/L7_taxa_list.txt | cut -f2`
@@ -174,12 +158,11 @@ Single representative sequence for this taxon.  Skipping alignment.
 	" > $outdir/Representative_sequences/L7_sequences_by_taxon_alignments/$taxid/$taxid\_log.txt
 	elif [[ $otu_count -ge 2 ]]; then
 	mkdir $outdir/Representative_sequences/L7_sequences_by_taxon_alignments/$taxid
-	mafft --localpair --maxiterate 1000 --quiet --nuc --reorder --treeout --clustalout --thread $threads $outdir/Representative_sequences/L7_sequences_by_taxon/$taxid.fasta > $outdir/Representative_sequences/L7_sequences_by_taxon_alignments/$taxid/$taxid\_aligned.aln
+	mafft --localpair --maxiterate 1000 --quiet --nuc --reorder --treeout --clustalout --thread $threads $outdir/Representative_sequences/L7_sequences_by_taxon/$taxid.fasta > $outdir/Representative_sequences/L7_sequences_by_taxon_alignments/$taxid/${taxid}_aligned.aln
 	fi
 done
 
 ## Generate summary stats
-
 	if [[ ! -f $outdir/Representative_sequences/otus_per_taxon_summary.txt ]]; then
 		workdir=$(pwd)
 		cd $outdir/Representative_sequences/
