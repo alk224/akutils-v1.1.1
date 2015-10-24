cc8e1cc0cabe64c67b6ec3b81ab1fa433dec49ef
alk224 <alk224@nau.edu>
2015-10-23 18:06:14 -0700

    updated for new nmds conversion code

--- nonnormalized_table_diversity_analyses.sh
+++ nonnormalized_table_diversity_analyses.sh
@@ -154,7 +154,7 @@ Principal coordinates and NMDS commands:" >> $log
 	nmds.py -i $dm -o $outdir/bdiv/$dmbase\_nmds.txt" >> $log
 	principal_coordinates.py -i $dm -o $outdir/bdiv/$dmbase\_pc.txt >/dev/null 2>&1 || true
 	nmds.py -i $dm -o $outdir/bdiv/$dmbase\_nmds.txt >/dev/null 2>&1 || true
-	python $scriptdir/convert_nmds_coords.py $outdir/bdiv/$dmbase\_nmds.txt $outdir/bdiv/$dmbase\_nmds_converted.txt
+	python $scriptdir/convert_nmds_coords.py -i $outdir/bdiv/$dmbase\_nmds.txt -o $outdir/bdiv/$dmbase\_nmds_converted.txt
 	done
 
 ## Make 3D emperor plots (PCoA)
